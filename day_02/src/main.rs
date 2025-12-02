1edd49166a924343d8ea9d0cfdccd0f508119260
Markus Sommer <markus@splork.de>
2025-12-02 23:01:01 +0100

    Turns out I underestimated the space complexity...
    So now we do it parallelized instead.

--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -1,18 +1,15 @@
-mod preprocessed;
-
 use fancy_regex::Regex;
 use std::path::Path;
+use std::sync::mpsc;
+use std::sync::mpsc::{Receiver, Sender};
+use std::thread;
 use std::{env, fs};
-use std::fs::File;
-use std::io::prelude::*;
 
 fn main() {
-    preprocessor(2000, "src/preprocessed.rs");
-
-    //let filename = env::args().nth(1).expect("Need filename");
-    //let input = read_input(filename);
-    //let (part_1, part_2) = solve(input);
-    //println!("{}\n{}", part_1, part_2);
+    let filename = env::args().nth(1).expect("Need filename");
+    let input = read_input(filename);
+    let (part_1, part_2) = solve_parallel(input);
+    println!("{}\n{}", part_1, part_2);
 }
 
 fn read_input<P>(filename: P) -> Vec<(u64, u64)>
@@ -35,14 +32,14 @@ where
     ranges
 }
 
-fn solve(input: Vec<(u64, u64)>) -> (u64, u64) {
+fn solve(input: &[(u64, u64)], tx: Sender<(u64, u64)>) {
     let regex_1 = Regex::new(r"^(.*)\1$").unwrap();
     let regex_2 = Regex::new(r"^(.*)\1+$").unwrap();
     let mut sum_1: u64 = 0;
     let mut sum_2: u64 = 0;
 
     for (start, stop) in input {
-        for n in start..stop + 1 {
+        for n in *start..=*stop {
             let n_str = n.to_string();
             if regex_1.is_match(&n_str).unwrap() {
                 sum_1 += n;
@@ -53,54 +50,32 @@ fn solve(input: Vec<(u64, u64)>) -> (u64, u64) {
         }
     }
 
-    (sum_1, sum_2)
+    tx.send((sum_1, sum_2)).unwrap();
 }
 
-fn solve_optimized(input: Vec<(u64, u64)>) -> (u64, u64) {
+fn solve_parallel(input: Vec<(u64, u64)>) -> (u64, u64) {
     let mut sum_1: u64 = 0;
     let mut sum_2: u64 = 0;
+    let cpus = num_cpus::get();
+    let chunk_size = input.len().div_ceil(cpus);
+    let chunks = input.chunks(chunk_size).map(|c| c.to_vec());
+    let n_chunks = chunks.len();
+    let (tx, rx): (Sender<(u64, u64)>, Receiver<(u64, u64)>) = mpsc::channel();
 
-    for (start, stop) in input {
-        sum_1 += preprocessed::preprocessed::PART_1[stop as usize] - preprocessed::preprocessed::PART_1[start as usize];
-        if preprocessed::preprocessed::PART_1[start as usize] != preprocessed::preprocessed::PART_1[(start-1) as usize] {
-            sum_1 += start;
-        }
+    for chunk in chunks {
+        let txc = tx.clone();
+        thread::spawn(move || {
+            solve(&chunk, txc);
+        });
+    }
 
-        sum_2 += preprocessed::preprocessed::PART_2[stop as usize] - preprocessed::preprocessed::PART_2[start as usize];
-        if preprocessed::preprocessed::PART_2[start as usize] != preprocessed::preprocessed::PART_2[(start-1) as usize] {
-            sum_2 += start;
-        }
+    for _ in 0..n_chunks {
+        let (part_1, part_2) = rx.recv().unwrap();
+        sum_1 += part_1;
+        sum_2 += part_2;
     }
 
     (sum_1, sum_2)
-}
-
-fn preprocessor<P>(n_max: u64, filename: P) where P: AsRef<Path>,{
-    let mut part_1: Vec<u64> = vec![0];
-    let mut part_2: Vec<u64> = vec![0];
-    let regex_1 = Regex::new(r"^(.*)\1$").unwrap();
-    let regex_2 = Regex::new(r"^(.*)\1+$").unwrap();
-    let mut sum_1: u64 = 0;
-    let mut sum_2: u64 = 0;
-
-    for n in 1..n_max+1 {
-        println!("{}", n);
-        let i = n as usize;
-        let n_str = n.to_string();
-
-        if regex_1.is_match(&n_str).unwrap() {
-            sum_1 += n;
-        }
-        if regex_2.is_match(&n_str).unwrap() {
-            sum_2 += n;
-        }
-
-        part_1.push(sum_1);
-        part_2.push(sum_2);
-    }
-
-    let mut file = File::create(filename).unwrap();
-    let _ = write!(file, "pub mod preprocessed {{\npub const PART_1: [u64; {}] = {:?};\npub const PART_2: [u64; {}] = {:?};\n}}", n_max+1, part_1, n_max+1, part_2);
 }
 
 #[cfg(test)]
@@ -110,14 +85,14 @@ mod tests {
     #[test]
     fn test_part_1_example() {
         let input = read_input("./example.txt");
-        let (part_1, _) = solve(input);
+        let (part_1, _) = solve_parallel(input);
         assert_eq!(part_1, 1227775554);
     }
 
     #[test]
     fn test_part_2_example() {
         let input = read_input("./example.txt");
-        let (_, part_2) = solve(input);
+        let (_, part_2) = solve_parallel(input);
         assert_eq!(part_2, 4174379265);
     }
 
@@ -125,7 +100,7 @@ mod tests {
     #[test]
     fn test_part_1_real() {
         let input = read_input("./input.txt");
-        let (part_1, _) = solve(input);
+        let (part_1, _) = solve_parallel(input);
         assert_eq!(part_1, 17077011375);
     }
 
@@ -133,15 +108,7 @@ mod tests {
     #[test]
     fn test_part_2_real() {
         let input = read_input("./input.txt");
-        let (_, part_2) = solve(input);
+        let (_, part_2) = solve_parallel(input);
         assert_eq!(part_2, 36037497037);
-    }
-
-    #[test]
-    fn test_preprocessor() {
-        let testdata: Vec<(u64, u64)> = vec![(11, 22), (95, 115), (998, 1012)];
-        let (part_1, part_2) = solve_optimized(testdata);
-        assert_eq!(part_1, 1142);
-        assert_eq!(part_2, 2252);
     }
 }
