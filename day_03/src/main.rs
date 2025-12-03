2f2ea1d86f41bb2f218ef5ca048fa25602ab9938
Markus Sommer <markus@splork.de>
2025-12-03 13:02:38 +0100

    Day 3, Part 2

--- day_03/src/main.rs
+++ day_03/src/main.rs
@@ -1,3 +1,4 @@
+use std::cmp::{max, min};
 use std::env;
 use std::fs::File;
 use std::io::{self, BufRead};
@@ -29,30 +30,53 @@ where
     ranges
 }
 
-fn solve(input: Vec<Vec<u8>>) -> (u32, u32) {
-    let mut part_1: u32 = 0;
-    let part_2: u32 = 0;
+fn solve(input: Vec<Vec<u8>>) -> (u64, u64) {
+    let mut part_1: u64 = 0;
+    let mut part_2: u64 = 0;
 
     for row in input {
-        let mut ten: u8 = row[0];
-        let mut ten_index: usize = 0;
-        let mut one: u8 = 0;
+        let mut digits_1: Vec<u8> = vec![row[0], 0];
+        let mut index_1: usize = 0;
+        let mut digits_2: Vec<u8> = vec![row[0], 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+        let mut index_2: usize = 0;
 
-        for (i, digit) in row[..row.len() - 1].iter().enumerate() {
-            if *digit > ten {
-                ten = *digit;
-                ten_index = i;
+        for digit_index in 0..digits_2.len() {
+            let mut max_1: usize = 0;
+            if digit_index < 2 {
+                max_1 = row.len() - (digits_1.len() - digit_index);
+            } else {
+                index_1 = 99;
+            }
+
+            let max_2 = row.len() - (digits_2.len() - digit_index);
+
+            let start = min(index_1 + 1, index_2 + 1);
+            let stop = max(max_1, max_2);
+
+            for i in start..=stop {
+                let n = row[i];
+                if (i > index_1) && (i <= max_1)
+                    && n > digits_1[digit_index] {
+                        digits_1[digit_index] = n;
+                        index_1 = i;
+                    }
+                if (i > index_2) && (i <= max_2)
+                    && n > digits_2[digit_index] {
+                        digits_2[digit_index] = n;
+                        index_2 = i;
+                    }
             }
         }
 
-        for digit in row[ten_index + 1..].iter() {
-            if *digit > one {
-                one = *digit;
-            }
-        }
+        let val_1: u64 = digits_1
+            .iter()
+            .fold(0, |acc, elem| acc * 10 + (*elem as u64));
+        part_1 += val_1;
 
-        let joltage: u8 = (ten * 10) + one;
-        part_1 += joltage as u32;
+        let val_2: u64 = digits_2
+            .iter()
+            .fold(0, |acc, elem| acc * 10 + (*elem as u64));
+        part_2 += val_2;
     }
 
     (part_1, part_2)
@@ -73,7 +97,7 @@ mod tests {
     fn test_part_2_example() {
         let input = read_input("./example.txt");
         let (_, part_2) = solve(input);
-        assert_eq!(part_2, 0);
+        assert_eq!(part_2, 3121910778619);
     }
 
     // this is for my personal puzzle input, yours will bit different
@@ -89,6 +113,6 @@ mod tests {
     fn test_part_2_real() {
         let input = read_input("./input.txt");
         let (_, part_2) = solve(input);
-        assert_eq!(part_2, 0);
+        assert_eq!(part_2, 171528556468625);
     }
 }
