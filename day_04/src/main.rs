fe5efdb27afbeccfc83fe6360f2b32a2a0d9d987
Markus Sommer <markus@splork.de>
2025-12-04 12:27:28 +0100

    Day 4, Part 1

---
+++ day_04/src/main.rs
@@ -0,0 +1,195 @@
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+use std::ops;
+
+struct Location {
+    row: usize,
+    column: usize,
+}
+
+impl ops::Add<Direction> for Location {
+    type Output = Result<Location, &'static str>;
+
+    fn add(self, _rhs: Direction) -> Result<Location, &'static str> {
+        let irow: isize = (self.row as isize) + _rhs.d_row;
+        let icolumn: isize = (self.column as isize) + _rhs.d_column;
+        if irow < 0 || icolumn < 0 {
+            return Err("Indices can't be < 0")
+        }
+
+        Ok(Location{
+            row: irow as usize,
+            column: icolumn as usize,
+        })
+    }
+}
+
+impl ops::Add<&Direction> for &Location {
+    type Output = Result<Location, &'static str>;
+
+    fn add(self, _rhs: &Direction) -> Result<Location, &'static str> {
+        let irow: isize = (self.row as isize) + _rhs.d_row;
+        let icolumn: isize = (self.column as isize) + _rhs.d_column;
+        if irow < 0 || icolumn < 0 {
+            return Err("Indices can't be < 0")
+        }
+
+        Ok(Location{
+            row: irow as usize,
+            column: icolumn as usize,
+        })
+    }
+}
+
+struct Direction {
+    d_row: isize,
+    d_column: isize,
+}
+
+impl ops::Add<Direction> for Direction {
+    type Output = Direction;
+
+    fn add(self, _rhs: Direction) -> Direction {
+        Direction{
+            d_row: self.d_row + _rhs.d_row,
+            d_column: self.d_column + _rhs.d_column,
+        }
+    }
+}
+
+impl ops::Mul<isize> for Direction {
+    type Output = Direction;
+
+    fn mul(self, _rhs: isize) -> Direction {
+        Direction{
+            d_row: self.d_row * _rhs,
+            d_column: self.d_column * _rhs,
+        }
+    }
+}
+
+const LEFT: Direction = Direction{d_row: 0, d_column: -1};
+const RIGHT: Direction = Direction{d_row: 0, d_column: 1};
+const UP: Direction = Direction{d_row: 1, d_column: 0};
+const DOWN: Direction = Direction{d_row: -1, d_column: 0};
+
+struct FloorPlan {
+    area: Vec<Vec<bool>>
+}
+
+impl FloorPlan {
+    fn check(&self, location: &Location) -> Result<bool, &'static str> {
+        if location.row >= self.area.len() {
+            return Err("Out of bounds")
+        }
+        let row = &self.area[location.row];
+        if location.column >= row.len() {
+            return Err("Out of bounds")
+        }
+        Ok(row[location.column])
+    }
+}
+
+type ParsedInput = (FloorPlan, Vec<Location>);
+
+fn main() {
+    let filename = env::args().nth(1).expect("Need filename");
+    let input = read_input(filename);
+    let (part_1, part_2) = solve(input);
+    println!("{}\n{}", part_1, part_2);
+}
+
+fn read_input<P>(filename: P) -> ParsedInput
+where
+    P: AsRef<Path>,
+{
+    let mut floorplan: Vec<Vec<bool>> = Vec::new();
+    let mut rolls: Vec<Location> = Vec::new();
+
+    let file = File::open(filename).unwrap();
+    for (row_index, line) in io::BufReader::new(file).lines().map(|l| l.unwrap()).enumerate() {
+        let mut row: Vec<bool> = Vec::new();
+        for (column_index, char) in line.chars().enumerate() {
+            match char {
+                '@' => {
+                    row.push(true);
+                    rolls.push(Location{row: row_index, column: column_index});
+                },
+                _ => row.push(false),
+            }
+        }
+        floorplan.push(row);
+    }
+
+    (FloorPlan{area: floorplan}, rolls)
+}
+
+fn solve(input: ParsedInput) -> (u32, u32) {
+    let mut part_1: u32 = 0;
+    let part_2: u32 = 0;
+    let (floorplan, rolls) = input;
+    let around: [Direction; 8] = [LEFT, LEFT+UP, UP, UP+RIGHT, RIGHT, RIGHT+DOWN, DOWN, DOWN+LEFT];
+
+    for roll in rolls.iter() {
+        let mut neighbours: u8 = 0;
+        for direction in around.iter() {
+            let step = roll+direction;
+            match step {
+                Ok(location) => {
+                    let check = floorplan.check(&location);
+                    match check {
+                        Ok(neighbour) => {
+                            if neighbour {
+                                neighbours += 1;
+                            }
+                        },
+                        Err(_) => {}
+                    }
+                },
+                Err(_) => {},
+            }
+        }
+        if neighbours < 4 {
+            part_1 += 1;
+        }
+    }
+
+    (part_1, part_2)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_1_example() {
+        let input = read_input("./example.txt");
+        let (part_1, _) = solve(input);
+        assert_eq!(part_1, 13);
+    }
+
+    #[test]
+    fn test_part_2_example() {
+        let input = read_input("./example.txt");
+        let (_, part_2) = solve(input);
+        assert_eq!(part_2, 0);
+    }
+
+    // this is for my personal puzzle input, yours will bit different
+    #[test]
+    fn test_part_1_real() {
+        let input = read_input("./input.txt");
+        let (part_1, _) = solve(input);
+        assert_eq!(part_1, 1416);
+    }
+
+    // this is for my personal puzzle input, yours will bit different
+    #[test]
+    fn test_part_2_real() {
+        let input = read_input("./input.txt");
+        let (_, part_2) = solve(input);
+        assert_eq!(part_2, 0);
+    }
+}
