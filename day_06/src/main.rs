5c3aeb3ddb5e70bc37e37542a06a6e14c04e9e0f
Markus Sommer <markus@splork.de>
2025-12-06 16:38:25 +0100

    Day 6, Part 2

--- day_06/src/main.rs
+++ day_06/src/main.rs
@@ -13,15 +13,17 @@ fn main() {
     println!("{}\n{}", part_1, part_2);
 }
 
-fn read_input<P>(filename: P) -> ParsedInput
+fn read_input<P>(filename: P) -> (ParsedInput, ParsedInput)
 where
     P: AsRef<Path>,
 {
     let mut numbers: Vec<Vec<u64>> = Vec::new();
     let mut operands: Vec<char> = Vec::new();
+    let mut chars: Vec<Vec<char>> = Vec::new();
 
     let file = File::open(filename).unwrap();
     for line in io::BufReader::new(file).lines().map(|l| l.unwrap()) {
+        chars.push(line.chars().collect());
         let mut line_numbers: Vec<u64> = Vec::new();
         let parts = line.split(" ");
         for part in parts {
@@ -39,33 +41,92 @@ where
         }
     }
 
-    let mut input: Vec<(char, Vec<u64>)> = Vec::with_capacity(operands.len());
+    let input_1 = zip_input(operands, numbers);
+    let input_2 = transform_input(chars);
+
+    (input_1, input_2)
+}
+
+fn transform_input(mut input: Vec<Vec<char>>) -> ParsedInput {
+    let max_len: usize = input.iter().map(|l| l.len()).max().unwrap();
+    for row in input.iter_mut() {
+        while row.len() < max_len {
+            row.push(' ');
+        }
+    }
+
+    let mut transposed: Vec<Vec<char>> = transpose_ragged(input);
+    let mut operands: Vec<char> = Vec::new();
+
+    let mut numbers: Vec<Vec<u64>> = Vec::new();
+    let mut line_numbers: Vec<u64> = Vec::new();
+    for column in transposed.iter_mut().rev() {
+        if let Some(&last) = column.last()
+            && (last == '+' || last == '*')
+        {
+            operands.push(last);
+            column.pop();
+        }
+        let num_str: String = column.iter().collect();
+        let trimmed = num_str.trim();
+        if trimmed.is_empty() {
+            numbers.push(line_numbers);
+            line_numbers = Vec::new();
+            continue;
+        }
+
+        line_numbers.push(trimmed.parse::<u64>().unwrap())
+    }
+    numbers.push(line_numbers);
+
+    let transformed: ParsedInput = zip(operands, numbers).collect();
+
+    transformed
+}
+
+fn zip_input(operands: Vec<char>, numbers: Vec<Vec<u64>>) -> ParsedInput {
+    let mut zipped: ParsedInput = Vec::with_capacity(operands.len());
     for operand in operands.iter() {
-        input.push((*operand, Vec::with_capacity(operands.len())));
+        zipped.push((*operand, Vec::with_capacity(operands.len())));
     }
 
     for line_numbers in numbers.iter() {
         for (i, (_, number)) in zip(&operands, line_numbers).enumerate() {
-            input[i].1.push(*number);
+            zipped[i].1.push(*number);
         }
     }
 
-    input
+    zipped
 }
 
-fn solve(input: ParsedInput) -> (u64, u64) {
-    let mut part_1: u64 = 0;
-    let part_2: u64 = 0;
+fn transpose_ragged<T: Copy>(v: Vec<Vec<T>>) -> Vec<Vec<T>> {
+    let rows = v.len();
+    let cols = v.iter().map(|row| row.len()).min().unwrap_or(0);
+
+    (0..cols)
+        .map(|c| (0..rows).map(|r| v[r][c]).collect())
+        .collect()
+}
+
+fn solve(input: (ParsedInput, ParsedInput)) -> (u64, u64) {
+    let part_1 = compute_solution(input.0);
+    let part_2 = compute_solution(input.1);
+
+    (part_1, part_2)
+}
+
+fn compute_solution(input: ParsedInput) -> u64 {
+    let mut sum: u64 = 0;
 
     for (operand, numbers) in input.iter() {
         if *operand == '+' {
-            part_1 += numbers.iter().sum::<u64>();
+            sum += numbers.iter().sum::<u64>();
         } else {
-            part_1 += numbers.iter().product::<u64>();
+            sum += numbers.iter().product::<u64>();
         }
     }
 
-    (part_1, part_2)
+    sum
 }
 
 #[cfg(test)]
@@ -83,7 +144,7 @@ mod tests {
     fn test_part_2_example() {
         let input = read_input("./example.txt");
         let (_, part_2) = solve(input);
-        assert_eq!(part_2, 0);
+        assert_eq!(part_2, 3263827);
     }
 
     // this is for my personal puzzle input, yours will bit different
@@ -99,6 +160,6 @@ mod tests {
     fn test_part_2_real() {
         let input = read_input("./input.txt");
         let (_, part_2) = solve(input);
-        assert_eq!(part_2, 0);
+        assert_eq!(part_2, 10695785245101);
     }
 }
