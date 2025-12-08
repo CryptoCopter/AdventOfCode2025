3234a4af40416613b904563f75a7845ce4eee1d1
Markus Sommer <markus@splork.de>
2025-12-08 16:15:36 +0100

    Day 7, Part 2

--- day_07/src/main.rs
+++ day_07/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, HashSet, VecDeque};
+use std::collections::{HashMap, VecDeque};
 use std::env;
 use std::fs::File;
 use std::io::{self, BufRead};
@@ -7,19 +7,104 @@ use std::path::Path;
 type Height = u8;
 type Column = u8;
 
+#[derive(Debug)]
+enum Side {
+    Left,
+    Right,
+}
+
+#[derive(Debug)]
+struct Beam {
+    start: Height,
+    column: Column,
+    origin: Option<Splitter>,
+    side: Option<Side>,
+}
+
 #[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
 struct Splitter {
     height: Height,
     column: Column,
 }
 
-#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
-struct Beam {
-    start: Height,
-    column: Column,
+struct SplitterTree {
+    root: Option<Splitter>,
+    indices: HashMap<Splitter, usize>,
+    nodes: Vec<TreeNode>,
 }
 
-type ParsedInput = (Beam, HashMap<Column, Vec<Splitter>>);
+impl SplitterTree {
+    fn insert(&mut self, node: TreeNode) -> Result<(), &'static str> {
+        if self.indices.contains_key(&node.splitter) {
+            return Err("Node already in tree");
+        }
+
+        if self.root.is_none() {
+            self.root = Some(node.splitter)
+        }
+
+        let node_index = self.nodes.len();
+
+        self.indices.insert(node.splitter, node_index);
+        self.nodes.push(node);
+
+        Ok(())
+    }
+
+    fn get(&self, splitter: &Splitter) -> Result<&TreeNode, &'static str> {
+        if let Some(node_index) = self.indices.get(splitter) {
+            Ok(&self.nodes[*node_index])
+        } else {
+            Err("Splitter not in Tree")
+        }
+    }
+
+    fn contains(&self, splitter: &Splitter) -> bool {
+        self.indices.contains_key(splitter)
+    }
+
+    fn connect(
+        &mut self,
+        parent: Splitter,
+        child: Splitter,
+        side: Side,
+    ) -> Result<(), &'static str> {
+        if let Some(parent_index) = self.indices.get(&parent) {
+            let parent_node = &mut self.nodes[*parent_index];
+
+            match side {
+                Side::Left => {
+                    if parent_node.left_child.is_some() {
+                        return Err("Child already occupied");
+                    }
+                    parent_node.left_child = Some(child);
+                }
+                Side::Right => {
+                    if parent_node.right_child.is_some() {
+                        return Err("Child already occupied");
+                    }
+                    parent_node.right_child = Some(child);
+                }
+            }
+        } else {
+            return Err("Parent not in tree");
+        }
+
+        Ok(())
+    }
+
+    fn size(&self) -> usize {
+        self.indices.len()
+    }
+}
+
+struct TreeNode {
+    splitter: Splitter,
+    left_child: Option<Splitter>,
+    right_child: Option<Splitter>,
+}
+
+type ParsedInput = (Column, HashMap<Column, Vec<Splitter>>);
 
 fn main() {
     let filename = env::args().nth(1).expect("Need filename");
@@ -32,70 +117,91 @@ fn read_input<P>(filename: P) -> ParsedInput
 where
     P: AsRef<Path>,
 {
-    let mut start: Beam = Beam {
-        start: 0,
-        column: 0,
-    };
-    let mut height: Height = 0;
-    let mut splitters: HashMap<Column, Vec<Splitter>> = HashMap::new();
+    let mut start: Column = 0;
+    let mut splitter_map: HashMap<Column, Vec<Splitter>> = HashMap::new();
 
     let file = File::open(filename).unwrap();
-    for line in io::BufReader::new(file).lines().map(|l| l.unwrap()) {
+    for (height, line) in io::BufReader::new(file)
+        .lines()
+        .map(|l| l.unwrap())
+        .enumerate()
+    {
         for (column, char) in line.chars().enumerate() {
             if char == 'S' {
-                start.column = column as Column;
+                start = column as Column;
             }
             if char == '^' {
                 let splitter = Splitter {
                     height: height as Height,
                     column: column as Column,
                 };
-                if let Some(splitters_column) = splitters.get_mut(&(column as Column)) {
+                if let Some(splitters_column) = splitter_map.get_mut(&(column as Column)) {
                     splitters_column.push(splitter);
                 } else {
                     let splitters_column: Vec<Splitter> = vec![splitter];
-                    splitters.insert(column as Column, splitters_column);
+                    splitter_map.insert(column as Column, splitters_column);
                 }
             }
         }
-
-        height += 1;
     }
 
-    (start, splitters)
+    (start, splitter_map)
 }
 
 fn solve(input: ParsedInput) -> (usize, u64) {
-    let part_2: u64 = 0;
-    let (start, mut splitters) = input;
+    let (start, splitter_map) = input;
+
+    let start_beam = Beam {
+        start: 0,
+        column: start,
+        origin: None,
+        side: None,
+    };
+
+    let mut tree: SplitterTree = SplitterTree {
+        root: None,
+        indices: HashMap::new(),
+        nodes: Vec::new(),
+    };
 
     let mut beams: VecDeque<Beam> = VecDeque::new();
-    beams.push_back(start);
-
-    let mut hit_splitters: HashSet<Splitter> = HashSet::new();
+    beams.push_back(start_beam);
 
     while !beams.is_empty() {
-        let ray = beams.pop_front().unwrap();
-        if let Some(column_splitters) = splitters.get_mut(&ray.column) {
+        let beam = beams.pop_front().unwrap();
+        if let Some(column_splitters) = splitter_map.get(&beam.column) {
             for splitter in column_splitters.iter() {
-                if splitter.height > ray.start {
-                    if hit_splitters.contains(splitter) {
-                        break;
+                if splitter.height > beam.start {
+                    if !tree.contains(splitter) {
+                        let node = TreeNode {
+                            splitter: *splitter,
+                            left_child: None,
+                            right_child: None,
+                        };
+                        tree.insert(node).unwrap();
+
+                        let beam_left = Beam {
+                            start: splitter.height,
+                            column: splitter.column - 1,
+                            origin: Some(*splitter),
+                            side: Some(Side::Left),
+                        };
+                        beams.push_back(beam_left);
+
+                        let beam_right = Beam {
+                            start: splitter.height,
+                            column: splitter.column + 1,
+                            origin: Some(*splitter),
+                            side: Some(Side::Right),
+                        };
+                        beams.push_back(beam_right);
                     }
 
-                    let ray_left = Beam {
-                        start: splitter.height,
-                        column: splitter.column - 1,
-                    };
-                    beams.push_back(ray_left);
-
-                    let ray_right = Beam {
-                        start: splitter.height,
-                        column: splitter.column + 1,
-                    };
-                    beams.push_back(ray_right);
-
-                    hit_splitters.insert(*splitter);
+                    if let Some(parent) = beam.origin
+                        && let Some(side) = beam.side
+                    {
+                        tree.connect(parent, *splitter, side).unwrap();
+                    }
 
                     break;
                 }
@@ -103,7 +209,40 @@ fn solve(input: ParsedInput) -> (usize, u64) {
         }
     }
 
-    (hit_splitters.len(), part_2)
+    (tree.size(), collapse_the_wavefunction(tree))
+}
+
+fn collapse_the_wavefunction(tree: SplitterTree) -> u64 {
+    let mut cache: HashMap<Splitter, u64> = HashMap::new();
+    tree_walk(&tree, tree.root.unwrap(), &mut cache)
+}
+
+fn tree_walk(tree: &SplitterTree, splitter: Splitter, cache: &mut HashMap<Splitter, u64>) -> u64 {
+    if let Some(cached) = cache.get(&splitter) {
+        return *cached;
+    }
+
+    let node = tree.get(&splitter).unwrap();
+
+    if node.left_child.is_none() && node.right_child.is_none() {
+        return 2;
+    }
+
+    let mut left: u64 = 1;
+    let mut right: u64 = 1;
+
+    if let Some(child) = node.left_child {
+        left = tree_walk(tree, child, cache);
+    }
+    if let Some(child) = node.right_child {
+        right = tree_walk(tree, child, cache);
+    }
+
+    let total = left + right;
+
+    cache.insert(splitter, total);
+
+    total
 }
 
 #[cfg(test)]
@@ -121,7 +260,7 @@ mod tests {
     fn test_part_2_example() {
         let input = read_input("./example.txt");
         let (_, part_2) = solve(input);
-        assert_eq!(part_2, 0);
+        assert_eq!(part_2, 40);
     }
 
     // this is for my personal puzzle input, yours will bit different
@@ -137,6 +276,6 @@ mod tests {
     fn test_part_2_real() {
         let input = read_input("./input.txt");
         let (_, part_2) = solve(input);
-        assert_eq!(part_2, 0);
+        assert_eq!(part_2, 95408386769474);
     }
 }
