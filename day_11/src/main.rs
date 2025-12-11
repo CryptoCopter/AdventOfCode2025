969d1d20a58920e3f1db4384f9616fde565e65a8
Markus Sommer <markus@splork.de>
2025-12-11 17:03:40 +0100

    Day 11, Part 1

---
+++ day_11/src/main.rs
@@ -0,0 +1,135 @@
+use std::collections::HashMap;
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+struct Device {
+    name: String,
+    outputs: Vec<String>,
+}
+
+struct Graph {
+    root: usize,
+    devices: Vec<Device>,
+    name_map: HashMap<String, usize>,
+}
+
+impl Graph {
+    fn new() -> Graph {
+        let mut graph = Graph {
+            root: 0,
+            devices: vec![Device {
+                name: "out".to_string(),
+                outputs: Vec::new(),
+            }],
+            name_map: HashMap::new(),
+        };
+
+        graph.name_map.insert("out".to_string(), 0);
+
+        graph
+    }
+
+    fn add(&mut self, device: Device) {
+        let index = self.devices.len();
+        let name: String = device.name.clone();
+
+        if name == "you" {
+            self.root = index;
+        }
+
+        self.name_map.insert(name, index);
+        self.devices.push(device);
+    }
+
+    fn walk(&self) -> u64 {
+        let mut cache: HashMap<String, u64> = HashMap::with_capacity(self.devices.len());
+        self._walk(&self.devices[self.root], &mut cache)
+    }
+
+    fn _walk(&self, device: &Device, cache: &mut HashMap<String, u64>) -> u64 {
+        if let Some(cached) = cache.get(&device.name) {
+            return *cached;
+        }
+
+        if device.name == "out" {
+            return 1;
+        }
+
+        let mut sum: u64 = 0;
+
+        for output in &device.outputs {
+            sum += self._walk(&self.devices[*self.name_map.get(output).unwrap()], cache)
+        }
+
+        cache.insert(device.name.clone(), sum);
+        sum
+    }
+}
+
+fn read_input<P>(filename: P) -> Graph
+where
+    P: AsRef<Path>,
+{
+    let mut graph = Graph::new();
+
+    let file = File::open(filename).unwrap();
+    for line in io::BufReader::new(file).lines().map(|l| l.unwrap()) {
+        let parts: Vec<&str> = line.split(": ").collect();
+        let name: String = parts[0].to_string();
+        let outputs: Vec<String> = parts[1].split(" ").map(|out| out.to_string()).collect();
+
+        let device = Device { name, outputs };
+
+        graph.add(device);
+    }
+
+    graph
+}
+
+fn solve(graph: Graph) -> (u64, u64) {
+    (graph.walk(), 0)
+}
+
+fn main() {
+    let filename = env::args().nth(1).expect("Need filename");
+    let input = read_input(filename);
+    let (part_1, part_2) = solve(input);
+    println!("{}\n{}", part_1, part_2);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_1_example() {
+        let input = read_input("./example.txt");
+        let (part_1, _) = solve(input);
+        assert_eq!(part_1, 5);
+    }
+
+    #[test]
+    fn test_part_2_example() {
+        let input = read_input("./example.txt");
+        let (_, part_2) = solve(input);
+        assert_eq!(part_2, 0);
+    }
+
+    // this is for my personal puzzle input, yours will bit different
+    #[test]
+    fn test_part_1_real() {
+        let input = read_input("./input.txt");
+        let (part_1, _) = solve(input);
+        assert_eq!(part_1, 543);
+    }
+
+    // this is for my personal puzzle input, yours will bit different
+    #[test]
+    fn test_part_2_real() {
+        let input = read_input("./input.txt");
+        let (_, part_2) = solve(input);
+        assert_eq!(part_2, 0);
+    }
+}
