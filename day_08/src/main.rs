2fda879109982dd03cd292645d41ddfad72241e7
Markus Sommer <markus@splork.de>
2025-12-11 13:24:22 +0100

    Day 8, Part 2

--- day_08/src/main.rs
+++ day_08/src/main.rs
@@ -26,22 +26,26 @@ struct Connection {
     b: Box,
 }
 
+impl Connection {
+    fn distance_wall(&self) -> u64 {
+        self.a.x as u64 * self.b.x as u64
+    }
+}
+
 #[derive(Clone)]
 struct Circuit {
+    index: usize,
     boxes: HashSet<Box>,
-    connections: HashSet<Connection>,
 }
 
 impl Circuit {
     fn add(&mut self, connection: Connection) {
         self.boxes.insert(connection.a);
         self.boxes.insert(connection.b);
-        self.connections.insert(connection);
     }
 
     fn merge(&mut self, other: Circuit) {
         self.boxes.extend(other.boxes);
-        self.connections.extend(other.connections);
     }
 
     fn size(&self) -> u64 {
@@ -52,27 +56,36 @@ impl Circuit {
 struct Forest {
     circuits: Vec<Circuit>,
     box_map: HashMap<Box, usize>,
+    active_circuits: HashSet<usize>,
 }
 
 impl Forest {
+    fn from_boxes(boxes: &[Box]) -> Forest {
+        let mut forest = Forest {
+            circuits: Vec::with_capacity(boxes.len()),
+            box_map: HashMap::with_capacity(boxes.len()),
+            active_circuits: HashSet::with_capacity(boxes.len()),
+        };
+
+        for (index, cbox) in boxes.iter().enumerate() {
+            let circuit = Circuit {
+                index,
+                boxes: HashSet::from([*cbox]),
+            };
+            forest.circuits.push(circuit);
+            forest.box_map.insert(*cbox, index);
+            forest.active_circuits.insert(index);
+        }
+
+        forest
+    }
+
     fn add(&mut self, connection: Connection) {
         let a_present = self.box_map.get(&connection.a).copied();
         let b_present = self.box_map.get(&connection.b).copied();
 
         match (a_present, b_present) {
-            (None, None) => {
-                // Neither box already in a circuit -> create new circuit
-                let new_index = self.circuits.len();
-                self.box_map.insert(connection.a, new_index);
-                self.box_map.insert(connection.b, new_index);
-
-                let mut new_circuit = Circuit {
-                    boxes: HashSet::new(),
-                    connections: HashSet::new(),
-                };
-                new_circuit.add(connection);
-                self.circuits.push(new_circuit);
-            }
+            (None, None) => (),
             (Some(index), None) => {
                 // box a already in a circuit -> add connection & extend circuit
                 self.box_map.insert(connection.b, index);
@@ -97,20 +110,25 @@ impl Forest {
                 }
 
                 self.circuits[index_a].merge(circuit_b);
+                self.active_circuits.remove(&index_b);
             }
         }
     }
 
-    fn largest(mut self) -> u64 {
-        self.circuits
-            .sort_unstable_by_key(|circuit| circuit.boxes.len());
-        self.circuits.reverse();
+    fn largest(&self) -> u64 {
+        let mut current_circuits = self.circuits.clone();
+        current_circuits.retain(|circ| self.active_circuits.contains(&circ.index));
 
-        self.circuits[0].size() * self.circuits[1].size() * self.circuits[2].size()
+        current_circuits.sort_unstable_by_key(|circuit| circuit.boxes.len());
+        current_circuits.reverse();
+
+        current_circuits[0].size() * current_circuits[1].size() * current_circuits[2].size()
+    }
+
+    fn dense(&self) -> bool {
+        self.active_circuits.len() == 1
     }
 }
-
-type ParsedInput = Vec<Box>;
 
 fn main() {
     let filename = env::args().nth(1).expect("Need filename");
@@ -119,7 +137,7 @@ fn main() {
     println!("{}\n{}", part_1, part_2);
 }
 
-fn read_input<P>(filename: P) -> ParsedInput
+fn read_input<P>(filename: P) -> Vec<Box>
 where
     P: AsRef<Path>,
 {
@@ -138,18 +156,25 @@ where
     boxes
 }
 
-fn solve(input: ParsedInput, n: usize) -> (u64, u64) {
+fn solve(input: Vec<Box>, n: usize) -> (u64, u64) {
+    let mut forest = Forest::from_boxes(&input);
     let connections = compute_connections(input);
-    let mut forest = Forest {
-        circuits: Vec::new(),
-        box_map: HashMap::new(),
-    };
 
+    // part 1
     for connection in connections.iter().take(n) {
         forest.add(*connection)
     }
+    let part_1 = forest.largest();
 
-    (forest.largest(), 0)
+    // continue with part 2
+    let mut index = n;
+    while !forest.dense() {
+        forest.add(connections[index]);
+        index += 1;
+    }
+    let last_connection = connections[index - 1];
+
+    (part_1, last_connection.distance_wall())
 }
 
 fn compute_connections(boxes: Vec<Box>) -> Vec<Connection> {
@@ -185,7 +210,7 @@ mod tests {
     fn test_part_2_example() {
         let input = read_input("./example.txt");
         let (_, part_2) = solve(input, 10);
-        assert_eq!(part_2, 0);
+        assert_eq!(part_2, 25272);
     }
 
     // this is for my personal puzzle input, yours will bit different
@@ -201,6 +226,6 @@ mod tests {
     fn test_part_2_real() {
         let input = read_input("./input.txt");
         let (_, part_2) = solve(input, 1000);
-        assert_eq!(part_2, 0);
+        assert_eq!(part_2, 9259958565);
     }
 }
